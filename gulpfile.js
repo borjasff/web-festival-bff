05f2e09cbca8b7fd351f1026ab3d8e6cc5a7a8fe
Borja Florin Fernandez <borjasff3@gmail.com>
2023-06-15 00:11:32 +0200

    festival-web

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const { src, dest, watch, parallel } = require('gulp');
+
+//css
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+//js
+
+const terser = require('gulp-terser-js');
+
+function css(done) {
+  src('src/scss/**/*.scss') //Identificar el Archivo SASS
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass()) //Compilarlo
+    .pipe(postcss([autoprefixer(),cssnano()]))
+    .pipe(sourcemaps.write('.'))//donde se guarda
+    .pipe(dest('build/css')) //Almacenarla en el disco duro
+  done(); //Callback avisa a gulp cuando llegamos al final
+}
+ 
+function imagenes(done){
+    const opciones = { 
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{jpg,png}').pipe(cache(imagemin(opciones))).pipe(dest('build/img'))
+    done();
+}
+
+function versionWebp(done) {
+  const opciones = {
+    quality: 40
+  };
+  src('src/img/**/*.{jpg,png}').pipe(webp(opciones)).pipe(dest('build/img'))
+  done();
+}
+ 
+function versionAvif(done) {
+    const opciones = {
+      quality: 40
+    };
+    src('src/img/**/*.{jpg,png}').pipe(avif(opciones)).pipe(dest('build/img'))
+    done();
+  }
+
+  //JS
+  function javascript(done){
+    src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/js'));
+    done();
+  }
+function dev(done) {
+  watch('src/scss/**/*.scss', css);
+  watch('src/js/**/*.js', javascript);
+  done();
+}
+ 
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
